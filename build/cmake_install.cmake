26668649d1f8c371f8ee1d474902c31d61eb4762
hikitooo <hcton2426@apcs.fitus.edu.vn>
2024-12-20 16:17:47 +0700

    commit 20/12/2024 16:17

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: D:/projects/ChessGame
+# Install script for directory: D:/projects/24125020_24125023/Final-Project-Chess
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -47,6 +47,6 @@ endif()
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-  file(WRITE "D:/projects/ChessGame/build/${CMAKE_INSTALL_MANIFEST}"
+  file(WRITE "D:/projects/24125020_24125023/Final-Project-Chess/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
